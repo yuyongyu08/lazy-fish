b9b8a010a6d523a786f2cbde302dafbdf883d315
yuyongyu <yuyongyu@jwzg.com>
2022-11-04 20:53:35 +0800

    add style

--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,10 @@
-const img = document.createElement('img');
-img.src = chrome.runtime.getURL('images/fish.gif');
-img.style.position = 'absolute';
-document.body.appendChild(img);
+const div = document.createElement('div');
+div.style.width = '100%';
+div.style.height = '100%';
+div.style.display = 'flex';
+div.style.flexDirection = 'column';
+div.style.textAlign = 'center';
+
+const snippet = `<image width="620px" height="620px" style="margin-left: 50%;translate: -50%;" src="${chrome.runtime.getURL('images/fish.gif')}"/><h2 style="margin-top: -350px">摸鱼~</h2>`;
+div.innerHTML = snippet;
+document.body.appendChild(div);
